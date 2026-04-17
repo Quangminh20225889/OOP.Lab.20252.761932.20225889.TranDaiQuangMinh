07bca9f20136b0cecff45781eeeba6224abc3294
Quang Minh <trandaiquangminh2004@gmail.com>
2026-04-17 23:13:22 +0700

    Refactor project structure using packages

--- Lab03/OtherProjects/Lab02/SourceCode/DigitalVideoDisc.java
+++ Lab03/OtherProjects/Lab02/SourceCode/DigitalVideoDisc.java
@@ -1,90 +1,58 @@
 package Lab03.OtherProjects.Lab02.SourceCode;
 
 public class DigitalVideoDisc {
-    private static int nbDigitalVideoDiscs = 0;
-
-    private int id;
     private String title;
     private String category;
     private String director;
     private int length;
     private float cost;
 
-    public int getId() {
-        return id;
-    }
-
-    public static int getNbDigitalVideoDiscs() {
-        return nbDigitalVideoDiscs;
-    }
-
     public String getTitle() {
-        return title;
-    }
+    return title;
+     }
 
     public String getCategory() {
-        return category;
-    }
+    return category;
+     }   
 
     public String getDirector() {
-        return director;
-    }
+    return director;
+     }
 
     public int getLength() {
-        return length;
-    }
+    return length;
+     }
 
     public float getCost() {
-        return cost;
-    }
+    return cost;
+     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    private void assignId() {
-        nbDigitalVideoDiscs++;
-        this.id = nbDigitalVideoDiscs;
-    }
-
+    // 1. Tạo DVD chỉ bằng title
     public DigitalVideoDisc(String title) {
-        assignId();
-        this.title = title;
-    }
+    this.title = title;
+     }
 
+// 2. Tạo DVD bằng title, category, cost
     public DigitalVideoDisc(String title, String category, float cost) {
-        assignId();
-        this.title = title;
-        this.category = category;
-        this.cost = cost;
-    }
+    this.title = title;
+    this.category = category;
+    this.cost = cost;
+     }
 
+// 3. Tạo DVD bằng title, category, director, cost
     public DigitalVideoDisc(String title, String category, String director, float cost) {
-        assignId();
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.cost = cost;
-    }
+    this.title = title;
+    this.category = category;
+    this.director = director;
+    this.cost = cost;
+     }
 
+// 4. Tạo DVD bằng tất cả thuộc tính
     public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
-        assignId();
-        this.title = title;
-        this.category = category;
-        this.director = director;
-        this.length = length;
-        this.cost = cost;
-    }
-
-    public boolean isMatch(String title) {
-        if (title == null) {
-            return false;
-        }
-        return this.title != null && this.title.equalsIgnoreCase(title.trim());
-    }
-
-    @Override
-    public String toString() {
-        return "DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
-    }
+    this.title = title;
+    this.category = category;
+    this.director = director;
+    this.length = length;
+    this.cost = cost;
+     } 
 }
--- Lab03/OtherProjects/Lab02/SourceCode/TestPassingParameter.java
+++ Lab03/OtherProjects/Lab02/SourceCode/TestPassingParameter.java
@@ -2,26 +2,23 @@ package Lab03.OtherProjects.Lab02.SourceCode;
 
 public class TestPassingParameter {
     public static void main(String[] args) {
-        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
-        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
-
-        swap(jungleDVD, cinderellaDVD);
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
-
-        changeTitle(jungleDVD, cinderellaDVD.getTitle());
-        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-    }
-
-    public static void swap(Object o1, Object o2) {
-        Object tmp = o1;
-        o1 = o2;
-        o2 = tmp;
-    }
-
-    public static void changeTitle(DigitalVideoDisc dvd, String title) {
-        String oldTitle = dvd.getTitle();
-        dvd.setTitle(title);
-        dvd = new DigitalVideoDisc(oldTitle);
+        
+        // Test các constructor khác nhau của DigitalVideoDisc
+        
+        // Constructor 1: chỉ title
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King");
+        System.out.println("DVD 1: " + dvd1.getTitle());
+        
+        // Constructor 2: title, category, cost
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", 24.95f);
+        System.out.println("DVD 2: " + dvd2.getTitle() + " - " + dvd2.getCategory());
+        
+        // Constructor 3: title, category, director, cost
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", "John Musker", 18.99f);
+        System.out.println("DVD 3: " + dvd3.getTitle() + " - " + dvd3.getDirector());
+        
+        // Constructor 4: tất cả thuộc tính
+        DigitalVideoDisc dvd4 = new DigitalVideoDisc("Interstellar", "Sci-Fi", "Christopher Nolan", 169, 29.99f);
+        System.out.println("DVD 4: " + dvd4.getTitle() + " - " + dvd4.getLength() + " mins");
     }
 }
